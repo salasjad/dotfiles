3bed2ddfd7016294923b3e33abf41fe690942c39
Salahuddin Asjad <asjaddev@gmail.com>
2016-09-20 13:50:45 +0200

    added ycm_build autcompleter

---
+++ ycm_build/ycm/tests/gmock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/salahuddin/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/gmock/gtest/src/gtest-all.cc" "/home/salahuddin/ycm_build/ycm/tests/gmock/gtest/CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_THREAD_DONT_USE_CHRONO"
+  "USE_CLANG_COMPLETER"
+  "YCMD_CORE_VERSION=24"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/salahuddin/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm"
+  "/home/salahuddin/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/ClangCompleter"
+  "/home/salahuddin/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts"
+  "/usr/include/python2.7"
+  "/home/salahuddin/ycm_temp/llvm_root_dir/include"
+  "/home/salahuddin/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/gmock/gtest/include"
+  "/home/salahuddin/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/gmock/gtest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
